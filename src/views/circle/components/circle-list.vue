0a47bf61d31b29b40770266d8ebc0c2b90bb8e6c
forever <743867239@qq.com>
2020-03-07 23:29:01 +0800

    圈子页面完成

---
+++ src/views/circle/components/circle-list.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="circle_content">
+    <div class="circle_head">
+      <img src="../../../assets/images/chat_p01.png" alt />
+      <div class="circle_info">
+        <p>英雄联盟那些事</p>
+        <span>15分钟前</span>
+      </div>
+      <span class="circle_follow">关注</span>
+    </div>
+    <div class="circle_body">
+      <div class="circle_text">LPL全明星周末11月30日海口冯小刚电影公社正式开幕</div>
+      <img src="../../../assets/images/chat_p07.png" alt />
+    </div>
+    <div class="circle_foot">
+      <ul>
+        <li>
+          <van-icon name="good-job-o" size="0.32rem" />
+          <span>36</span>
+        </li>
+        <li>
+          <van-icon name="chat-o" size="0.32rem" />
+          <span>1020</span>
+        </li>
+        <li>
+          <van-icon name="good-job-o" size="0.32rem" />
+          <span>3020</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.circle_content {
+  padding: 0.3rem;
+  border-top: 0.2rem solid rgba(244, 244, 244, 1);
+  .circle_head {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    img {
+      width: 0.92rem;
+      height: 0.92rem;
+      border-radius: 50%;
+    }
+    .circle_info {
+      position: absolute;
+      left: 1.1rem;
+      display: flex;
+      flex-direction: column;
+      p {
+        margin-bottom: 0.12rem;
+        font-size: 0.28rem;
+        font-family: Microsoft YaHei;
+        font-weight: bold;
+        color: rgba(249, 94, 95, 1);
+      }
+      span {
+        font-size: 0.2rem;
+        font-family: Microsoft YaHei;
+        font-weight: 400;
+        color: rgba(77, 77, 77, 1);
+      }
+    }
+    .circle_follow {
+      padding: 0.1rem 0.3rem;
+      border: 1px solid rgba(239, 118, 118, 1);
+      border-radius: 0.22rem;
+    }
+    .circle_unfollow {
+      font-size: 0.24rem;
+      font-family: Microsoft YaHei;
+      font-weight: 400;
+      color: rgba(179, 179, 179, 1);
+    }
+  }
+  .circle_body {
+    .circle_text {
+      margin: 0.29rem 0 0.4rem;
+      font-size: 0.28rem;
+      font-family: Microsoft YaHei;
+      font-weight: 400;
+      color: rgba(51, 51, 51, 1);
+    }
+    img {
+      width: 100%;
+      height: 3.5rem;
+    }
+  }
+  .circle_foot {
+    ul {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      margin: 0.4rem 0 0.32rem;
+      li {
+        display: flex;
+        align-items: center;
+        span {
+          margin-left: 0.12rem;
+          font-size: 0.24rem;
+          font-family: Microsoft YaHei;
+          font-weight: 400;
+          color: rgba(102, 102, 102, 1);
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/circle/index.vue
+++ src/views/circle/index.vue
@@ -1,50 +1,226 @@
 <template>
   <com-page-tabbar-wrapper>
     <van-nav-bar class="circle-navbar" fixed border title="圈子" @touchmove.prevent.stop.native>
-    <div class="circle-navbar__right" slot="right">
-      <div class="image-wrapper" @click="onSearch">
-        <img class="image" src="@/assets/images/circle/search.png" alt="搜索">
+      <div class="circle-navbar__right" slot="right">
+        <div class="image-wrapper" @click="onSearch">
+          <img class="image" src="@/assets/images/circle/search.png" alt="搜索" />
+        </div>
+        <div class="image-wrapper" @click="onWrite">
+          <img class="image" src="@/assets/images/circle/write.png" alt="搜索" />
+        </div>
       </div>
-      <div class="image-wrapper" @click="onWrite">
-        <img class="image" src="@/assets/images/circle/write.png" alt="搜索">
+    </van-nav-bar>
+    <div class="info-container">
+      <ul class="circle_meun">
+        <li
+          :class="{active:currentNav==item.value}"
+          v-for="(item,index) in meunList"
+          :key="index"
+          @click="onNavToggle(item.value)"
+        >{{item.name}}</li>
+      </ul>
+      <cube-scroll ref="scroll" class="scroll" direction="horizontal">
+        <!-- 推荐 -->
+        <div class="circle_recommend">
+          <ul>
+            <li class="recommend">推荐</li>
+            <li v-for="(item,index) in recommendList" :key="index">
+              <img :src="item.url" />
+              <p>{{item.name}}</p>
+              <span>关注</span>
+            </li>
+          </ul>
+        </div>
+      </cube-scroll>
+      <div class="tab__panel-wrapper">
+        <com-loadmore :fetchData="query">
+          <template slot-scope="{list}">
+            <circle-list v-for="(item,index) in list" :key="index" :item="index"></circle-list>
+          </template>
+        </com-loadmore>
       </div>
     </div>
-    </van-nav-bar>
   </com-page-tabbar-wrapper>
 </template>
 
 <script>
+import CircleList from "./components/circle-list";
 export default {
+  components: {
+    CircleList
+  },
+  data() {
+    return {
+      meunList: [
+        {
+          name: "全部",
+          value: 1
+        },
+        {
+          name: "关注",
+          value: 2
+        }
+      ],
+      currentNav: 1,
+      recommendList: [
+        {
+          name: "王者赛事圈",
+          url: require("../../assets/images/chat_p01.png")
+        },
+        {
+          name: "王者赛事圈",
+          url: require("../../assets/images/chat_p01.png")
+        },
+        {
+          name: "王者赛事圈",
+          url: require("../../assets/images/chat_p01.png")
+        },
+        {
+          name: "赛事直播",
+          url: require("../../assets/images/chat_p02.png")
+        },
+        {
+          name: "LOL战场",
+          url: require("../../assets/images/chat_p03.png")
+        },
+        {
+          name: "赛事直播",
+          url: require("../../assets/images/chat_p02.png")
+        },
+        {
+          name: "LOL战场",
+          url: require("../../assets/images/chat_p03.png")
+        },
+        { name: "LOL战场", url: require("../../assets/images/chat_p04.png") }
+      ]
+    };
+  },
   methods: {
     onSearch() {
-      console.log('onSearch')
+      console.log("onSearch");
     },
     onWrite() {
-      console.log('onWrite')
+      console.log("onWrite");
+    },
+    onNavToggle(value) {
+      this.currentNav = value;
+    },
+    query() {
+      // mock
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(Array(10).fill(null));
+        }, 1500);
+      });
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-  .circle-navbar {
-    height: $header-height;
-    line-height: $header-height;
-    &:after {
-      border-color: #e6e6e6;
-    }
+.scroll /deep/ .cube-scroll-content {
+  display: inline-block;
+}
+.circle-navbar {
+  height: $header-height;
+  line-height: $header-height;
+  &:after {
+    border-color: #e6e6e6;
   }
-  .circle-navbar__right {
+}
+.circle-navbar__right {
+  display: flex;
+  .image-wrapper {
+    @include extend-click();
+  }
+  .image-wrapper + .image-wrapper {
+    margin-left: 22px;
+  }
+  .image {
+    width: 22px;
+    height: 22px;
+  }
+}
+
+.tab__panel-wrapper {
+  height: calc(100vh - 0.88rem);
+  overflow: hidden;
+}
+
+.info-container {
+  width: 100%;
+  position: absolute;
+  top: 0.88rem;
+  display: flex;
+  flex-direction: column;
+  .circle_meun {
+    width: 100%;
     display: flex;
-    .image-wrapper {
-      @include extend-click();
+    align-items: center;
+    justify-content: space-around;
+    li {
+      padding: 0.37rem 0.2rem 0.24rem;
+      font-size: 0.28rem;
+      font-family: Microsoft YaHei;
+      font-weight: 400;
+      color: rgba(77, 77, 77, 1);
     }
-    .image-wrapper + .image-wrapper {
-      margin-left: 22px;
-    }
-    .image {
-      width: 22px;
-      height: 22px;
+    .active {
+      position: relative;
+      color: rgba(249, 94, 95, 1);
+      &::before {
+        position: absolute;
+        bottom: 0;
+        display: inline-block;
+        content: "";
+        width: 0.55rem;
+        height: 0.04rem;
+        background: rgba(249, 94, 95, 1);
+      }
     }
   }
+  .circle_recommend {
+    display: flex;
+    padding-left: 0.32rem;
+    margin: 0.3rem 0;
+    ul {
+      display: flex;
+      align-items: center;
+      .recommend {
+        width: 0.96rem;
+        text-align: center;
+        font-size: 0.28rem;
+        font-family: Microsoft YaHei;
+        font-weight: bold;
+        color: rgba(179, 179, 179, 1);
+      }
+      li {
+        width: 1.4rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        img {
+          width: 1.14rem;
+          height: 1.14rem;
+        }
+        p {
+          margin: 0.18rem 0 0.23rem;
+          font-size: 0.24rem;
+          font-family: Microsoft YaHei;
+          font-weight: 400;
+          color: rgba(77, 77, 77, 1);
+        }
+        span {
+          padding: 0.1rem 0.3rem;
+          border: 1px solid rgba(239, 118, 118, 1);
+          border-radius: 0.22rem;
+          font-size: 0.24rem;
+          font-family: Microsoft YaHei;
+          font-weight: 400;
+          color: rgba(249, 94, 95, 1);
+        }
+      }
+    }
+  }
+}
 </style>
