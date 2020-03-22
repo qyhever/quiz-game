51f78906e08112e558f8f65e5116c1ca2d57c2b9
query <15927700473@163.com>
2020-03-22 10:52:52 +0800

    线上接口地址修改

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 const baseURL = {
   dev: '/api',
-  prod: 'http://120.24.224.218:8080'
+  prod: 'http://120.24.224.218:8081/api'
+  // prod: 'http://120.24.224.218:8080'
 }[process.env.VUE_APP_MODE]
 
 export default baseURL
